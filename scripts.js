b5dc77384298ffdca1f3da0993d50076192c996f
isaacainamaani <isaac.aina2002@gmail.com>
2023-03-01 13:16:35 +0300

    added a condition

--- scripts.js
+++ scripts.js
@@ -1,26 +1,24 @@
-let futureTime = new Date("January 1, 2024 00:00:00").getTime()
+let futureTime = new Date("March 2, 2023 00:00:00").getTime()
+
 
 var countdown = setInterval(()=>{
     let now = new Date().getTime()
     let timeLeft = futureTime - now;
-
-    let days = Math.floor(timeLeft/(1000*60*60*24));
-    let hours = Math.floor((timeLeft%(1000*60*60*24))/(1000*60*60));
-    let minutes = Math.floor((timeLeft%(1000*60*60))/(1000*60));
-    let seconds = Math.floor((timeLeft%(1000*60))/1000);
-
-    document.getElementById('days').innerHTML = days
-    document.getElementById('hours').innerHTML = hours
-    document.getElementById('minutes').innerHTML = minutes
-    document.getElementById('seconds').innerHTML = seconds
+    if(timeLeft>0){
+        let days = Math.floor(timeLeft/(1000*60*60*24));
+        let hours = Math.floor((timeLeft%(1000*60*60*24))/(1000*60*60));
+        let minutes = Math.floor((timeLeft%(1000*60*60))/(1000*60));
+        let seconds = Math.floor((timeLeft%(1000*60))/1000);
     
-    if(timeleft<=0){
-        clearInterval(countdown)
-        document.getElementById('days').innerHTML = 00
-        document.getElementById('hours').innerHTML = 00
-        document.getElementById('minutes').innerHTML = 00
-        document.getElementById('seconds').innerHTML = 00
-    
-    }
+        
+        document.getElementById('days').innerHTML = formatTime(days)
+        document.getElementById('hours').innerHTML = formatTime(hours)
+        document.getElementById('minutes').innerHTML = formatTime(minutes)
+        document.getElementById('seconds').innerHTML = formatTime(seconds)
+    }  
 
 },1000)
+
+function formatTime(time){
+    return time<10 ? `0${time}` : time
+}
